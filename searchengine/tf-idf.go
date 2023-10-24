ed1e0d15c33dbd615a31109fbba2cbba9a8de1c7
ismail debbarh <usamdh07@gmail.com>
2023-10-24 16:41:53 +0100

    feat: add tf-idf algorithm

--- searchengine/tf-idf.go
+++ searchengine/tf-idf.go
@@ -1,4 +1,31 @@
 package searchengine
 
-func calcTf() {
+import (
+	"math"
+)
+
+func calcTF(tf TermsFrequency, t string) int {
+	n, ok := tf[t] // term freq on specific document
+	if !ok {
+		n = 0
+	}
+	N := len(tf) // number of terms in specific document
+	return n / N
+}
+
+func calcIDF(ftf FilesTermsFrequency, t string) float64 {
+	n := 0        // number of document that have this term
+	N := len(ftf) // number of document
+	for _, tf := range ftf {
+		_, ok := tf[t]
+		if ok {
+			n += 1
+		}
+	}
+
+	if n == 0 { // to avoid divition on zero if the term apeare in non document
+		n += 1
+	}
+
+	return math.Log10(float64(N / n))
 }
