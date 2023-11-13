2e90e235e9bb9109fe7aa2f2b0957f8407dc6c07
ismail debbarh <usamdh07@gmail.com>
2023-11-13 14:24:27 +0100

    fix: prev save dir files info because .Info() return the current info state

--- searchengine/folder-listener.go
+++ searchengine/folder-listener.go
@@ -2,9 +2,23 @@ package searchengine
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"time"
 )
+
+func getEntriesInfo(entries []fs.DirEntry) []fs.FileInfo {
+	entriesInfo := []fs.FileInfo{}
+	for _, e := range entries {
+		entrieInfo, err := e.Info()
+		if err != nil {
+			fmt.Printf("ERROR: Could not get info of %s : %v", e.Name(), err)
+			os.Exit(1)
+		}
+		entriesInfo = append(entriesInfo, entrieInfo)
+	}
+	return entriesInfo
+}
 
 func FolderListener(folderPath string) {
 	fi, err := os.Stat(folderPath)
@@ -20,11 +34,13 @@ func FolderListener(folderPath string) {
 		os.Exit(1)
 	}
 
-	prevFolderEntries, err := os.ReadDir(folderPath)
+	prevEntries, err := os.ReadDir(folderPath)
 	if err != nil {
 		fmt.Printf("Error: could not get the entries of: %s: %s", folderPath, err)
 		os.Exit(1)
 	}
+
+	prevFolderEntries := getEntriesInfo(prevEntries)
 
 	fmt.Printf("listening on : %s\n", folderPath)
 
@@ -37,32 +53,32 @@ func FolderListener(folderPath string) {
 
 		if len(newFolderEntries) < len(prevFolderEntries) {
 			fmt.Println("file deleted!!!!")
+			prevFolderEntries = getEntriesInfo(newFolderEntries)
 		} else if len(newFolderEntries) > len(prevFolderEntries) {
 			fmt.Println("new file added!!!!")
+			prevFolderEntries = getEntriesInfo(newFolderEntries)
 		} else if len(newFolderEntries) == len(prevFolderEntries) {
 			for i, curNewFileState := range newFolderEntries {
-				curPrevFileState := prevFolderEntries[i]
-				curPrevFileInfo, err := curPrevFileState.Info()
+				curPrevFileInfo := prevFolderEntries[i]
 				if err != nil {
-					fmt.Printf("ERROR: Could not get info of %s : %v", folderPath+"/"+curPrevFileState.Name(), err)
+					fmt.Printf("ERROR: Could not get info of %s : %v", curPrevFileInfo.Name(), err)
 					os.Exit(1)
 				}
 
 				curNewFileInfo, err := curNewFileState.Info()
 				if err != nil {
-					fmt.Printf("ERROR: Could not get info of %s : %v", folderPath+"/"+curNewFileState.Name(), err)
+					fmt.Printf("ERROR: Could not get info of %s : %v", curNewFileState.Name(), err)
 					os.Exit(1)
 				}
 
 				if !curPrevFileInfo.ModTime().Equal(curNewFileInfo.ModTime()) {
 					fmt.Printf("file %s / %s is changed!!!!\n", folderPath, curPrevFileInfo.Name())
+					prevFolderEntries = getEntriesInfo(newFolderEntries)
 				}
 			}
 		}
 
-		prevFolderEntries = newFolderEntries
-
-		time.Sleep(2 * time.Second) // Add a delay between iterations
+		time.Sleep(1 * time.Second)
 
 	}
 }
